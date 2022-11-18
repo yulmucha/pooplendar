1717545ddbccbd781d17e47b48724f536fb1ed3a
Yul Jeon <coffeeuyul@gmail.com>
2022-11-18 09:55:34 +0900

    docs(board): create asciidoc for board and convert adoc to html

--- src/test/kotlin/pooplendar/community/board/BoardRestControllerTest.kt
+++ src/test/kotlin/pooplendar/community/board/BoardRestControllerTest.kt
@@ -4,6 +4,10 @@ import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.test.web.servlet.post
 import pooplendar.community.RestControllerTest
 import pooplendar.community.board.application.BoardService
@@ -27,6 +31,26 @@ class BoardRestControllerTest : RestControllerTest() {
         }.andExpect {
             status { isCreated() }
             content { success(response) }
+        }.andDo {
+            handle(
+                MockMvcRestDocumentation.document(
+                    "board-create",
+                    PayloadDocumentation.requestFields(
+                        fieldWithPath("name").type(JsonFieldType.STRING)
+                            .description("게시판 이름"),
+                    ),
+                    PayloadDocumentation.responseFields(
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description(""),
+                        fieldWithPath("body").type(JsonFieldType.OBJECT)
+                            .description(""),
+                        fieldWithPath("body.id").type(JsonFieldType.NUMBER)
+                            .description("게시판 아이디"),
+                        fieldWithPath("body.name").type(JsonFieldType.STRING)
+                            .description("게시판 이름"),
+                    )
+                )
+            )
         }
     }
 }
